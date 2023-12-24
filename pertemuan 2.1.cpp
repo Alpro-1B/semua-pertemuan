1e2b24e4731c19e7d4f298633134bdec831d7948
bedul130723 <151449928+bedul130723@users.noreply.github.com>
2023-12-24 19:35:41 +0700

    Add files via upload

---
+++ pertemuan 2.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+     // Penamaan Variabel Untuk Jumlah Bebek (N) dan Jumlah Teman (M)
+     int N, M;
+
+     // Meminta User Untuk Memasukan Jumlah Bebek
+     cout << "Masukkan jumlah bebek (N): ";
+     cin >> N;
+
+     // Meminta User Untuk Memasukan Jumlah Teman
+     cout << "Masukkan jumlah teman (M): ";
+     cin >> M;
+
+     // Menghitung Banyak Bebek Yang Didapatkan Masing-Masing Teman Dengan Cara Membagi Jumlah Bebek (N) Dengan Jumlah Teman (M)
+     int bebekPerTeman = N / M;
+     // Menghitung Banyak Bebek Pak Tatang Yang Tersisa Dengan Metode Modulus (Sisa Bagi = Sisa Bebek)
+     int sisaBebek = N % M;
+
+     // Menampilakan Banyak Bebek Yang Didapat Teman Pak Tatang
+     cout << "Setiap teman akan mendapatkan : " << bebekPerTeman << " ekor bebek." << endl;
+     // Menampilkan Sisa Bebek Pak Tatang
+     cout << "Sisa bebek yang tidak dibagi : " << sisaBebek << " ekor bebek.";
+
+     return 0;
+}
